40505cd68e751e2aceee4032c14c206c6eb8ff3a
DANDA VENKATA SATHWIK REDDY <165892101+DVSR1411@users.noreply.github.com>
2024-11-05 12:47:52 +0530

    Update jenkins.sh

--- AWS/create-ec2/jenkins/jenkins.sh
+++ AWS/create-ec2/jenkins/jenkins.sh
@@ -6,5 +6,6 @@ rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum upgrade -y
 dnf install java-17-amazon-corretto -y
 yum install jenkins -y
+echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 systemctl enable jenkins
 systemctl start jenkins
