868804f48e5ae78cf9d90ada0435b69dc4821692
DANDA VENKATA SATHWIK REDDY <165892101+DVSR1411@users.noreply.github.com>
2025-02-05 16:41:46 +0530

    Update apigateway.tf

--- AWS/lambda/lambdaapi/apigateway.tf
+++ AWS/lambda/lambdaapi/apigateway.tf
@@ -7,13 +7,13 @@ resource "aws_api_gateway_rest_api" "my_rest_api" {
 }
 resource "aws_api_gateway_resource" "api_resource" {
   rest_api_id = aws_api_gateway_rest_api.my_rest_api.id
-  parent_id   = aws_api_gateway_rest_api.my_rest_api.root_resource_id
-  path_part   = "test"
+  parent_id = aws_api_gateway_rest_api.my_rest_api.root_resource_id
+  path_part = "test"
 }
 resource "aws_api_gateway_method" "post_method" {
-  rest_api_id   = aws_api_gateway_rest_api.my_rest_api.id
-  resource_id   = aws_api_gateway_resource.api_resource.id
-  http_method   = "POST"
+  rest_api_id = aws_api_gateway_rest_api.my_rest_api.id
+  resource_id = aws_api_gateway_resource.api_resource.id
+  http_method = "POST"
   authorization = "NONE"
 }
 resource "aws_api_gateway_method_response" "post_method_response" {
@@ -22,9 +22,9 @@ resource "aws_api_gateway_method_response" "post_method_response" {
   http_method = aws_api_gateway_method.post_method.http_method
   status_code = "200"
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin"  = true,
     "method.response.header.Access-Control-Allow-Headers" = true,
     "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin" = true,
     "method.response.header.Access-Control-Allow-Credentials" = true
   }
 }
@@ -36,57 +36,28 @@ resource "aws_api_gateway_integration" "lambda" {
   integration_http_method = "POST"
   uri                     = aws_lambda_function.test_lambda.invoke_arn
 }
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id = "AllowExecutionFromAPIGateway"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.test_lambda.function_name
+  principal = "apigateway.amazonaws.com"
+  source_arn = "${aws_api_gateway_rest_api.my_rest_api.execution_arn}/*/*"
+}
 resource "aws_api_gateway_integration_response" "post_integration_response" {
   rest_api_id = aws_api_gateway_rest_api.my_rest_api.id
   resource_id = aws_api_gateway_resource.api_resource.id
   http_method = aws_api_gateway_method.post_method.http_method
   status_code = aws_api_gateway_method_response.post_method_response.status_code
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin"  = "'*'",
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
     "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'",
+    "method.response.header.Access-Control-Allow-Origin" = "'*'",
     "method.response.header.Access-Control-Allow-Credentials" = "'true'"
   }
-}
-resource "aws_api_gateway_method" "options_method" {
-  rest_api_id   = aws_api_gateway_rest_api.my_rest_api.id
-  resource_id   = aws_api_gateway_resource.api_resource.id
-  http_method   = "OPTIONS"
-  authorization = "NONE"
-}
-resource "aws_api_gateway_method_response" "options_response" {
-  rest_api_id = aws_api_gateway_rest_api.my_rest_api.id
-  resource_id = aws_api_gateway_resource.api_resource.id
-  http_method = aws_api_gateway_method.options_method.http_method
-  status_code = "200"
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin"  = true,
-    "method.response.header.Access-Control-Allow-Headers" = true,
-    "method.response.header.Access-Control-Allow-Methods" = true,
-    "method.response.header.Access-Control-Allow-Credentials" = true
-  }
-}
-resource "aws_api_gateway_integration" "options" {
-  rest_api_id = aws_api_gateway_rest_api.my_rest_api.id
-  resource_id = aws_api_gateway_resource.api_resource.id
-  http_method = aws_api_gateway_method.options_method.http_method
-  type        = "MOCK"
-  request_templates = {
-    "application/json" = "{\"statusCode\": 200}"
-  }
-}
-
-resource "aws_api_gateway_integration_response" "options_integration_response" {
-  rest_api_id = aws_api_gateway_rest_api.my_rest_api.id
-  resource_id = aws_api_gateway_resource.api_resource.id
-  http_method = aws_api_gateway_method.options_method.http_method
-  status_code = aws_api_gateway_method_response.options_response.status_code
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin"  = "'*'",
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'",
-    "method.response.header.Access-Control-Allow-Credentials" = "'true'"
-  }
+  depends_on = [
+    aws_api_gateway_method.post_method,
+    aws_api_gateway_integration.lambda_integration
+  ]
 }
 resource "aws_api_gateway_deployment" "api_deployment" {
   rest_api_id = aws_api_gateway_rest_api.my_rest_api.id
@@ -94,13 +65,11 @@ resource "aws_api_gateway_deployment" "api_deployment" {
     create_before_destroy = true
   }
 }
-
 resource "aws_api_gateway_stage" "api_stage" {
   deployment_id = aws_api_gateway_deployment.api_deployment.id
   rest_api_id   = aws_api_gateway_rest_api.my_rest_api.id
   stage_name    = "prod"
 }
-
 resource "aws_lambda_permission" "api_gw" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
@@ -108,7 +77,6 @@ resource "aws_lambda_permission" "api_gw" {
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.my_rest_api.execution_arn}/*/*"
 }
-
 output "api_endpoint" {
   value = "${aws_api_gateway_stage.api_stage.invoke_url}/test"
 }
