8a89c53e7617edad33c9898e0ea8e62d700a9122
DANDA VENKATA SATHWIK REDDY <165892101+DVSR1411@users.noreply.github.com>
2025-03-30 13:45:55 +0530

    Update role.tf

--- AWS/sagemaker/role.tf
+++ AWS/sagemaker/role.tf
@@ -38,18 +38,26 @@ resource "aws_iam_policy" "mypolicy" {
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
-        {
-            Effect = "Allow"
-            Action = [
-                "s3:GetObject",
-                "s3:PutObject", 
-                "s3:DeleteObject",
-                "s3:ListBucket"
-            ]
-            Resource = [
-                "arn:aws:s3:::*"
-            ]
-        }
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::SageMaker"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Resource = [
+          "arn:aws:s3:::SageMaker/*"
+        ]
+      }
     ]
-    })
+  })
 }
