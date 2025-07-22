3cc9de0ca2ac4c0ba37e6b64e7d3df8880157205
DANDA VENKATA SATHWIK REDDY <165892101+DVSR1411@users.noreply.github.com>
2025-07-22 13:53:41 +0530

    Update main.tf

--- AWS/sagemaker/main.tf
+++ AWS/sagemaker/main.tf
@@ -2,7 +2,7 @@ resource "aws_sagemaker_notebook_instance" "myni" {
   name          = "demo"
   role_arn      = aws_iam_role.myrole.arn
   instance_type = "ml.t2.medium"
-  volume_size   = 5
+  volume_size   = 20
   subnet_id     = local.random_subnet_id
   security_groups = [aws_security_group.mysg.id]
   tags = {
